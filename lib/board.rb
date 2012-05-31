90c27e9f5ebb3a20a183192cda063a87e894267f
Tyler Green <tyler.green2@gmail.com>
2012-05-31 17:56:51 -0500

    Added basic board methods

--- lib/board.rb
+++ lib/board.rb
@@ -1,24 +1,58 @@
-class ArrayBoard
-  def initialize
-    @board = [:empty].cycle(9).to_a
+class ProtoBoard
+
+  def initalize(size)
+    @size = size
+  end
+
+  def available?(position)
+    @board[position] == :empty 
+  end
+
+  def legal_move?(position)
+    ((@size ** 2) - 1) >= position and position >= 0 and available?(position)
+  end
+  
+  def place(marker, position)
+    @board[position] = marker
+    self
+  end
+  
+end
+
+class ArrayBoard < ProtoBoard
+  def initialize(length)
+    @length = length
+    @size = length ** 2
+    @board = [:empty].cycle(@size).to_a
   end
 
   def to_a
     @board
   end
   
-  def available?(i)
-    @board[i] == :empty 
+  def rows
+    @board.each_slice(3).to_a
   end
 
-  def legal_move?(i)
-    8 >= i and i >= 0 and available?(i)
+  def columns
+    @board.each_with_index.
+      group_by {|x,i| i % @length }.
+      values.
+      flatten(1).
+      map{ |x,i| x }.
+      each_slice(3).
+      to_a
   end
   
-    
+  def diagonals
+    d1 = Range.new(0, @size).step(@length + 1).map{ |pos| @board[pos] }
+    d2 = Range.new(@length - 1, @size - @length).step(@length - 1).map{ |pos| @board[pos] }
+    [d1,d2]
+  end
+   
 end
 
-class HashBoard
+class HashBoard < ProtoBoard
   def initialize
     @board = Hash.new(:empty)
   end
@@ -27,14 +61,6 @@ class HashBoard
     @board
   end
 
-  def available?(i)
-    @board[i] == :empty 
-  end
-
-  def legal_move?(i)
-    8 >= i and i >= 0 and available(i)
-  end
-  
 end
 
 class Board < ArrayBoard
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -5,22 +5,46 @@ require 'board'
 describe Board do
   describe "board functionality" do
     it "is empty when created" do
-      assert_equal(Board.new.to_a, [:empty].cycle(9).to_a())
+      assert_equal(Board.new(3).to_a, [:empty].cycle(9).to_a())
     end
     
     it "knows if a spot is available" do
-      assert Board.new.available?(rand(9))
+      assert Board.new(3).available?(rand(9))
     end
 
     it "knows if a move is legal" do
-      assert Board.new.legal_move?(0)
+      (0..8).each do |pos|
+        assert(Board.new(3).legal_move?(pos), "failed for #{pos}")        
+      end
+      assert not(Board.new(3).place(:x,0).legal_move?(0))
     end
 
     it "can have pieces placed on it" do 
-      # Board.new.place()      
+      assert not(Board.new(3).place(:x,0).legal_move?(0))
+      assert([:x, :o, :empty], Board.new(3).place(:x,0).place(:o,3).rows.first )
+    end
+
+    it "provides access to rows" do
+      assert_equal([[:empty, :empty, :empty]].cycle(3).to_a, Board.new(3).rows)
+      assert([:x, :o, :empty], Board.new(3).place(:x,3).place(:o,4).rows[2] )
+    end
+    
+    it "provides access to columns" do
+      assert_equal([[:empty, :empty, :empty]].cycle(3).to_a, Board.new(3).columns)
+      assert_equal([:x, :o, :x], Board.new(3).place(:x, 0).place(:o, 3).place(:x, 6).columns.first)
+      assert_equal([:x, :o, :x], Board.new(3).place(:x, 1).place(:o, 4).place(:x, 7).columns[1])
     end
   end
+
+  it "provides access to its diagonals" do
+    assert_equal([[:x, :x, :empty], [:empty, :x, :o]],
+                 Board.new(3).place(:x, 0).place(:x, 4).place(:o, 6).diagonals)
+  end
+
+  
+    
 end
+
     
     
   
